b3518312ba3b2b147780a120fe08b8b558e7aa27
murali03031995 <151819037+murali03031995@users.noreply.github.com>
2024-06-13 19:02:42 +0530

    testing terraform count

--- servers.tf
+++ servers.tf
@@ -1,6 +1,7 @@
 # EC2 Public Server
-count = 3
-resource "aws_instance" "lms-web-server" {   
+
+resource "aws_instance" "lms-web-server" { 
+  count = 3
   ami           = "ami-0e001c9271cf7f3b9" # Ubuntu AMI 
   instance_type = "t2.micro"
   subnet_id = aws_subnet.lms-pub-sn.id
@@ -8,6 +9,6 @@ resource "aws_instance" "lms-web-server" {
   vpc_security_group_ids = [aws_security_group.lms-pub-sg.id]
 
   tags = {
-    Name = "lms-web-server"
+    Name = "lms-web-${count.index}"
   }
 }
