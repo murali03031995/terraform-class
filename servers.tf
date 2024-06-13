0f242a2554d246a7612b0b1dbbcefe1d630d4ff2
murali03031995 <151819037+murali03031995@users.noreply.github.com>
2024-06-13 18:51:52 +0530

    Create servers.tf

---
+++ servers.tf
@@ -0,0 +1,12 @@
+# EC2 Public Server
+resource "aws_instance" "lms-web-server" {   
+  ami           = "ami-0e001c9271cf7f3b9" # Ubuntu AMI 
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.lms-pub-sn.id
+  key_name = "krishna"
+  vpc_security_group_ids = [aws_security_group.lms-pub-sg.id]
+
+  tags = {
+    Name = "lms-web-server"
+  }
+}
